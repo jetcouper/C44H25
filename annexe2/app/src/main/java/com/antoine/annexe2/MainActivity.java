ede7c2d886f48549676568bdebe44cfe1f09550b
jetcouper <Yoshi9603!?Git>
2025-02-05 14:17:36 -0500

    Mercredi PM

--- annexe2/app/src/main/java/com/antoine/annexe2/MainActivity.java
+++ annexe2/app/src/main/java/com/antoine/annexe2/MainActivity.java
@@ -1,9 +1,11 @@
 package com.antoine.annexe2;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,13 +13,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.w3c.dom.Text;
+
+import java.util.Vector;
+
 public class MainActivity extends AppCompatActivity {
 
     EditText chamNomCompte;
     TextView champSolde;
     Button boutonValider;
 
+    Button boutonEnvoyer;
+    EditText champCourriel;
+    EditText champTransfère;
 
+    Vector<String> choix;
+    double solde;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +42,88 @@ public class MainActivity extends AppCompatActivity {
         });
 
         chamNomCompte = findViewById(R.id.txtDe);
+        champSolde = findViewById(R.id.txtSolde);
+        champCourriel = findViewById(R.id.txtA);
+        champTransfère = findViewById(R.id.txtTransfere);
+        boutonValider = findViewById(R.id.btnValider);
+        boutonEnvoyer = findViewById(R.id.btnEnvoyer);
+
+        solde = 1400;
+        choix = new Vector<>();
+        choix.add("CHEQUE");
+        choix.add("EPARGNE");
+        choix.add("EPARGNEPLUS");
+
+
+
+        //1ere étape
+        Ecouteur ec = new Ecouteur();
+
+        //2e étape
+        boutonValider.setOnClickListener(ec);
+
+        boutonEnvoyer.setOnClickListener(ec);
+
+
 
     }
+
+    //3e étape : Classe interne
+    private class Ecouteur implements View.OnClickListener{
+
+
+        @Override
+        public void onClick(View source) { //Paramètre: Source de l'événement, boutons
+
+            if (source == boutonValider){
+                //Équivalant
+                //String nomCompte = chamNomCompte.getText().toString();
+                String nomCompte = String.valueOf(chamNomCompte.getText());
+                nomCompte = nomCompte.toUpperCase(); //Place en majuscule pour faire moins de comparaisons
+                nomCompte.trim();//Pour enlever les espace inutiles au début ou à la fin du champ texte
+
+                if (choix.contains(nomCompte)){
+                    champSolde.setText(solde + "$");
+
+                }
+                else{
+                    //context: sysnonyme de l'activité
+                    Toast.makeText(MainActivity.this, "Pas un bon nom de compte!", Toast.LENGTH_LONG).show();
+                }
+            }
+            if (source == boutonEnvoyer){
+                //bouton envoyer
+                String nomCourriel = String.valueOf(champCourriel.getText());
+                nomCourriel = nomCourriel.trim();
+
+                //Pattern p = Pattern.compile(".+@.+\\.[a-z]+");
+
+                if (nomCourriel.isEmpty()){
+                    Toast.makeText(MainActivity.this, "Vous devez remplir le champ d'envoie!", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    double soldeDouble = Double.parseDouble(String.valueOf(champTransfère.getText()));
+
+                    if(solde == 0 || solde < soldeDouble){
+                        Toast.makeText(MainActivity.this, "La somme demander dépasse le solde du compte!", Toast.LENGTH_LONG).show();
+                    }
+                    else{
+                        solde = solde - soldeDouble;
+                        champSolde.setText(solde + "$");
+                        Toast.makeText(MainActivity.this, "Transfère réussi!", Toast.LENGTH_LONG).show();
+                    }
+
+
+                }
+            }
+
+
+
+        }
+
+
+    }
+
+
+
 }
