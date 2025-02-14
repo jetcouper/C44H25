c7c649271b8271c027a338a522d47b4a13767e18
jetcouper <Yoshi9603!?Git>
2025-02-14 10:38:34 -0500

    Vendredi

---
+++ annexe4b/app/src/main/java/com/antoine/annexe4b/MainActivity.java
@@ -0,0 +1,94 @@
+package com.antoine.annexe4b;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView chamPass;
+
+    LinearLayout main;
+
+    String input = "";
+
+    String test = "1234";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        //1er etape
+        Ecouteur ec = new Ecouteur();
+
+        //2e etape
+        main = findViewById(R.id.main);
+        chamPass = findViewById(R.id.txtPassword);
+        for(int i = 0; i < main.getChildCount(); i++){
+            LinearLayout ly = (LinearLayout) main.getChildAt(i);
+            for(int j = 0; j < ly.getChildCount(); j++){
+
+                View petit =  ly.getChildAt(j);
+                if(petit instanceof Button){
+                    ly.getChildAt(j).setOnClickListener(ec);
+                }
+            }
+        }
+
+
+    }
+
+    private class Ecouteur implements View.OnClickListener {
+        @Override
+        public void onClick(View source) {
+            //View c'est la superclasse de toutes les composantes Android
+            Button bouton = (Button)source; //On le transtype car on est certains que nos sources sont uniquement des Boutons
+            input += bouton.getText().toString();//On le transtype en Button car on veut avoir accès au Texte
+
+            if(chamPass.getText().length() != 4){
+                chamPass.setText(input);
+                main.setBackgroundColor(Color.WHITE);
+
+            }
+            else if(chamPass.getText().length() == 4){
+
+                if(chamPass.getText().toString().equals(test)){
+                    main.setBackgroundColor(Color.GREEN);
+                    Toast.makeText(MainActivity.this, "Vous avez trouver le code!!!", Toast.LENGTH_LONG).show();
+                    chamPass.setText("");
+                    input = "";
+                }
+                else{
+                    main.setBackgroundColor(Color.RED);
+                    Toast.makeText(MainActivity.this, "Vous vous êtes trompé!!!", Toast.LENGTH_LONG).show();
+                    chamPass.setText("");
+                    input = "";
+                }
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ annexe5/app/src/main/java/com/antoine/annexe2/MainActivity.java
@@ -0,0 +1,149 @@
+package com.antoine.annexe2;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import org.w3c.dom.Text;
+
+import java.text.DecimalFormat;
+import java.util.Vector;
+import java.util.regex.Pattern;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText chamNomCompte;
+    TextView champSolde;
+    Button boutonValider;
+
+    Button boutonEnvoyer;
+    EditText champCourriel;
+    EditText champTransfère;
+
+    Vector<String> choix;
+    double solde;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        chamNomCompte = findViewById(R.id.txtDe);
+        champSolde = findViewById(R.id.txtSolde);
+        champCourriel = findViewById(R.id.txtA);
+        champTransfère = findViewById(R.id.txtTransfere);
+        boutonValider = findViewById(R.id.btnValider);
+        boutonEnvoyer = findViewById(R.id.btnEnvoyer);
+
+        solde = 1400;
+        choix = new Vector<>();
+        choix.add("CHEQUE");
+        choix.add("EPARGNE");
+        choix.add("EPARGNEPLUS");
+
+
+
+        //1ere étape
+        Ecouteur ec = new Ecouteur();
+
+        //2e étape
+        boutonValider.setOnClickListener(ec);
+        boutonEnvoyer.setOnClickListener(ec);
+
+
+
+    }
+
+    public static boolean isValid(String email) { ///AJOUT PERSONNEL (Aucun lien avec le cours)
+
+        // Regular expression to match valid email formats
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+
+        // Compile the regex
+        Pattern p = Pattern.compile(emailRegex);
+
+        // Check if email matches the pattern
+        return email != null && p.matcher(email).matches();
+    }
+
+
+    //3e étape : Classe interne
+    private class Ecouteur implements View.OnClickListener{
+
+
+        @Override
+        public void onClick(View source) { //Paramètre: Source de l'événement, boutons
+
+            DecimalFormat df = new DecimalFormat("0.00$");
+            if (source == boutonValider){
+                //Équivalant
+                //String nomCompte = chamNomCompte.getText().toString();
+                String nomCompte = String.valueOf(chamNomCompte.getText());
+                nomCompte = nomCompte.toUpperCase(); //Place en majuscule pour faire moins de comparaisons
+                nomCompte.trim();//Pour enlever les espace inutiles au début ou à la fin du champ texte
+
+                if (choix.contains(nomCompte)){
+                    champSolde.setText(df.format( solde ));
+
+                }
+                else{
+                    //context: sysnonyme de l'activité
+                    Toast.makeText(MainActivity.this, "Pas un bon nom de compte!", Toast.LENGTH_LONG).show();
+                }
+            }
+            if (source == boutonEnvoyer){
+                //bouton envoyer
+                String nomCourriel = String.valueOf(champCourriel.getText());
+                nomCourriel = nomCourriel.trim();
+
+
+
+                if (nomCourriel.isEmpty()){
+                    Toast.makeText(MainActivity.this, "Vous devez remplir le champ d'envoie!", Toast.LENGTH_LONG).show();
+                }
+                else if (!isValid(nomCourriel)){
+                    Toast.makeText(MainActivity.this, "Ceci n'est pas un adresse Email valide!", Toast.LENGTH_LONG).show();
+
+                }
+                else{
+                    double soldeDouble = Double.parseDouble(String.valueOf(champTransfère.getText()));
+
+                    if(solde == 0 || solde < soldeDouble){
+                        Toast.makeText(MainActivity.this, "La somme demander dépasse le solde du compte!", Toast.LENGTH_LONG).show();
+                    }
+                    else{
+                        solde = solde - soldeDouble;
+                        champSolde.setText(df.format( solde ));
+                        Toast.makeText(MainActivity.this, "Transfère réussi!", Toast.LENGTH_LONG).show();
+                    }
+
+
+                }
+            }
+
+
+
+        }
+
+
+    }
+
+
+
+}
