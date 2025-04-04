5a546aa3aaf74946f4fe4243b7117b1e6741540f
jetcouper <Yoshi9603!?Git>
2025-04-04 08:35:19 -0400

    ec

---
+++ Examen2/app/src/main/java/antoine/dextraze/examen2/ItemMinecraft.java
@@ -0,0 +1,38 @@
+package antoine.dextraze.examen2;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+
+public class ItemMinecraft {
+
+    private int couleur,largeurTrait;
+
+    private String type;
+    private Paint ligneDessin;
+
+    public ItemMinecraft(int couleur, int largeurTrait, String type) {
+        this.couleur = couleur;
+        this.largeurTrait = largeurTrait;
+        this.type = type;
+
+
+
+        ligneDessin = new Paint(Paint.ANTI_ALIAS_FLAG);
+        ligneDessin.setColor(this.couleur);
+        ligneDessin.setStrokeWidth(this.largeurTrait);
+        ligneDessin.setAntiAlias(true);
+        ligneDessin.setStyle(Paint.Style.STROKE);
+    }
+
+    public void dessiner(Canvas canvas){
+
+        if(type == "Bambou"){
+
+            canvas.drawLine(500,0,500,400,ligneDessin);
+
+        }
+
+    }
+
+
+}
