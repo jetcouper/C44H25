c09653f4d454d17db13038f3f506e224d8bf631d
jetcouper <Yoshi9603!?Git>
2025-04-04 08:44:59 -0400

    asdf

--- Examen2/app/src/main/java/antoine/dextraze/examen2/MainActivity.java
+++ Examen2/app/src/main/java/antoine/dextraze/examen2/MainActivity.java
@@ -1,9 +1,12 @@
 package antoine.dextraze.examen2;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,11 +16,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
+
 public class MainActivity extends AppCompatActivity {
 
     ConstraintLayout conteneur;
     SurfaceDessin surf;
-
+    ChipGroup chipTypes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,13 +35,23 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
+        conteneur = findViewById(R.id.conteneur);
+        chipTypes = findViewById(R.id.groupe);
+        //Peut ne pas marcher
+        conteneur.setBackgroundColor(getColor((Integer)Color.GRAY));
 
         surf = new SurfaceDessin(this);
         surf.setLayoutParams(new ViewGroup.LayoutParams(-1,-1));
         conteneur.addView(surf);
+        Ecouteur ec = new Ecouteur();
+        for(int i = 0; i < chipTypes.getChildCount(); i++){
+            View petit = chipTypes.getChildAt(i);
+            if(petit instanceof Chip){
+                ((Chip)chipTypes.getChildAt(i)).setOnCheckedChangeListener(ec);
+            }
+        }
 
-
+        surf.setOnTouchListener(ec);
     }
 
     private class SurfaceDessin extends View {
@@ -43,6 +59,18 @@ public class MainActivity extends AppCompatActivity {
             super(context);
 
 
+        }
+    }
+
+    private class Ecouteur implements CompoundButton.OnCheckedChangeListener, View.OnTouchListener {
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+        }
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            return false;
         }
     }
 }
