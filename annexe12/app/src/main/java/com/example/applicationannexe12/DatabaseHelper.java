5c627862c56df97f22b94b6c89ae83c25b6586a0
jetcouper <Yoshi9603!?Git>
2025-04-23 14:20:42 -0400

    Mercredi 23-04-2025

--- annexe12/app/src/main/java/com/example/applicationannexe12/DatabaseHelper.java
+++ annexe12/app/src/main/java/com/example/applicationannexe12/DatabaseHelper.java
@@ -1,14 +1,22 @@
 package com.example.applicationannexe12;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
-public class DatabaseHelper extends SQLiteOpenHelper {
+import java.util.List;
+import java.util.Vector;
 
-    private static DatabaseHelper instance;
+public class DatabaseHelper extends SQLiteOpenHelper { //Singleton
+
+    private static DatabaseHelper instance; //Pour le singleton
+
+    //Autre variables
+    private SQLiteDatabase database;
 
     public static DatabaseHelper getInstance(Context contexte) {
         if(instance == null){
@@ -21,13 +29,77 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         super(contexte, "bd", null, 1);
     }
 
+    //Appeler une seule fois lors de l'installation de l'app.
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE IF NOT EXISTS 'Inventeur'('_id INTEGER' PRIMARY KEY AUTOINCREMENT,'nom' TEXT,'origine' TEXT,'invention' TEXT,'annee' INTEGER)");
+        db.execSQL("CREATE TABLE IF NOT EXISTS inventeur(_id INTEGER PRIMARY KEY AUTOINCREMENT,nom TEXT,origine TEXT,invention TEXT,annee INTEGER)");
+        ajouterInventeur(new Inventeur("Laszlo Biro","Hongrie", "Stylo à bille",1938),db);
+        ajouterInventeur(new Inventeur("Benjamin Franklin","Etats-Unis", "Paratonnerre",1752),db);
+        ajouterInventeur(new Inventeur("Mary Anderson","Etats-Unis", "Essuie-glace",1903),db);
+        ajouterInventeur(new Inventeur("Grace Hopper","Etats-Unis", "Compilateur",1952),db);
+        ajouterInventeur(new Inventeur("Benoit Rouquayrot","France", "Scaphandre",1864),db);
     }
-
+    public void ajouterInventeur(Inventeur in , SQLiteDatabase db){
+        ContentValues cv = new ContentValues();
+        cv.put("nom", in.getNom());
+        cv.put("origine", in.getOrigine());
+        cv.put("invention", in.getInvention());
+        cv.put("annee", in.getAnnee());
+        db.insert("inventeur",null,cv);
+    }
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+        //Si jamais on demande une mise à jour, on va seulement detruire la table et la recréer
+        db.execSQL("DROP TABLE IF EXISTS inventeur");
+        onCreate(db);
     }
+    public void ouvrirConnexion(){
+        database = this.getWritableDatabase();
+    }
+
+    public void fermerConnexion(){
+        database.close();
+    }
+
+    public Vector<String> retourerInvention()
+    {
+        Vector<String> inventions = new Vector<>();
+
+        Cursor cursor = database.rawQuery("SELECT invention FROM inventeur", null);
+        //Remplir le vecteur avec les inventions
+
+        //Tant qu'il y a des résultats
+        while (cursor.moveToNext()) {
+            //Je vais chercher la valeur du champ invention
+            String temp = cursor.getString(0);
+            inventions.add(temp);
+        }
+
+        //Je ferme le curseur
+        cursor.close();
+        //Je retourne le Vector rempli
+        return inventions;
+
+    }
+
+    public boolean aBonneReponse(String nomInventeur, String invention){
+
+
+        String[] tab = {nomInventeur,invention};
+        Cursor c = database.rawQuery("SELECT * FROM inventeur AS i WHERE i.nom = ? AND i.invention = ?", tab);
+//        if(c.moveToFirst()){
+//            return true;
+//        }
+//        else
+//            return false;
+        c.close();
+        return c.moveToFirst();
+    }
+
+
+
+
+
+
 }
--- annexe12/app/src/main/java/com/example/applicationannexe12/MainActivity.java
+++ annexe12/app/src/main/java/com/example/applicationannexe12/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.applicationannexe12;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +12,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Vector;
+
 public class MainActivity extends AppCompatActivity {
 
+    ListView liste;
+    DatabaseHelper instance;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +28,49 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        liste = findViewById(R.id.listInventaire);
+
+        instance = DatabaseHelper.getInstance(getApplicationContext()); // Pas this car le singleton est vivant pour toute l'application
+
+        instance.ouvrirConnexion(); //Très important
+        Vector<String> v = instance.retourerInvention();
+        //Remplir le ListView
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,v);
+
+        liste.setAdapter(adapter);
+
+        Ecouteur ec = new Ecouteur();
+
+        liste.setOnItemClickListener(ec);
+
+
     }
+    private class Ecouteur implements AdapterView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            
+
+
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        instance.fermerConnexion();
+
+    }
+
+
 }
+
+
+
+
+
+
+
+
