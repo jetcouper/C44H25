1a4fbf558deb320ef0fc4ad49c0259564a1288d9
jetcouper <Yoshi9603!?Git>
2025-05-07 14:58:55 -0400

    sadfg

--- TP2/app/src/main/java/com/example/tp2/Fin.java
+++ TP2/app/src/main/java/com/example/tp2/Fin.java
@@ -1,6 +1,8 @@
 package com.example.tp2;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Fin extends AppCompatActivity {
+
+    ListView listPoint;
+    Button quitter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +26,12 @@ public class Fin extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        listPoint = findViewById(R.id.listView);
+        quitter = findViewById(R.id.btnMenu);
+        
+
+
 
 
 
