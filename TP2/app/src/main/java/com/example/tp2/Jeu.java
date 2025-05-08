2ef8e3df2b95b40b330f26e5307da05282d88ee2
jetcouper <Yoshi9603!?Git>
2025-05-08 15:06:48 -0400

    Jeudi

--- TP2/app/src/main/java/com/example/tp2/Jeu.java
+++ TP2/app/src/main/java/com/example/tp2/Jeu.java
@@ -1,10 +1,12 @@
 package com.example.tp2;
 
+import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.DragEvent;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.LinearLayout;
@@ -43,19 +45,61 @@ public class Jeu extends AppCompatActivity {
         instance.ouvrirConnexion();
         Ecouteur ec = new Ecouteur();
 
+//        lC1 = findViewById(R.id.la1);
+//        lC2 = findViewById(R.id.la2);
+//        lC3 = findViewById(R.id.la3);
+//        lC4 = findViewById(R.id.la4);
+//        lC5 = findViewById(R.id.la5);
+//        lC6 = findViewById(R.id.la6);
+//        lC7 = findViewById(R.id.la7);
+//        lC8 = findViewById(R.id.la8);
 
-        for(int i = 0; i < main.getChildCount(); i++){
-            LinearLayout colonne = (LinearLayout) main.getChildAt(i);
-            colonne.setOnDragListener(ec);
-            colonne.getChildAt(0).setOnTouchListener(ec);
-        }
 
 
+//        for(int i = 0; i < main.getChildCount(); i++){
+//            LinearLayout colonne = (LinearLayout) main.getChildAt(i);
+//            String nom = getResources().getResourceName(colonne.getId());
+//            if(nom.contains("la")){
+//                colonne.setOnDragListener(ec);
+//                colonne.getChildAt(0).setOnTouchListener(ec);
+//            }
+//
+//        }
 
+
+        appliquerListeners(findViewById(R.id.main), ec, ec);
+        menuPrincipale.setOnClickListener(ec);
 
 
     }
-    private class Ecouteur implements View.OnDragListener, View.OnTouchListener {
+    private void appliquerListeners(View view, View.OnDragListener dragListener, View.OnTouchListener touchListener) {
+        if (view instanceof LinearLayout) {
+            LinearLayout layout = (LinearLayout) view;
+
+            int id = layout.getId();
+            if (id != View.NO_ID) {
+                try {
+                    String nom = getResources().getResourceEntryName(id);
+                    if (nom != null && !nom.isEmpty() && nom.contains("la")) {
+                        layout.setOnDragListener(dragListener);
+                        layout.setOnTouchListener(touchListener);  // <- C'est ici qu'on met le OnTouchListener
+                    }
+                } catch (Resources.NotFoundException ignored) {}
+            }
+        }
+
+        // Explorer récursivement les enfants
+        if (view instanceof ViewGroup) {
+            ViewGroup groupe = (ViewGroup) view;
+            for (int i = 0; i < groupe.getChildCount(); i++) {
+                appliquerListeners(groupe.getChildAt(i), dragListener, touchListener);
+            }
+        }
+    }
+
+
+
+    private class Ecouteur implements View.OnDragListener, View.OnTouchListener, View.OnClickListener {
         Drawable normal = getResources().getDrawable(R.drawable.bg_card, null);
         Drawable select = getResources().getDrawable(R.drawable.bg_card_selectionne, null);
 
@@ -76,6 +120,11 @@ public class Jeu extends AppCompatActivity {
             source.setVisibility(View.INVISIBLE); //Cacher le jeton car on est en train de le déplacer
 
             return true;
+        }
+
+        @Override
+        public void onClick(View v) {
+            finish();
         }
     }
 
