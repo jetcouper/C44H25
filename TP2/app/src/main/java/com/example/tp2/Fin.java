b282785b2e146e396f3f719f5ffd1271b6a32013
jetcouper <Yoshi9603!?Git>
2025-05-08 12:40:54 -0400

    Mise à jours des cartes

--- TP2/app/src/main/java/com/example/tp2/Fin.java
+++ TP2/app/src/main/java/com/example/tp2/Fin.java
@@ -1,8 +1,11 @@
 package com.example.tp2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +13,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Vector;
+
 public class Fin extends AppCompatActivity {
 
     ListView listPoint;
     Button quitter;
+    DatabaseHelper instance;
 
 
     @Override
@@ -30,14 +36,48 @@ public class Fin extends AppCompatActivity {
         listPoint = findViewById(R.id.listView);
         quitter = findViewById(R.id.btnMenu);
         
+        instance = DatabaseHelper.getInstance(getApplicationContext());
 
-
+        Vector<String> v = null;
+        try {
+            v = instance.retourerPointages();
+            if (v == null || v.isEmpty()) {
+                Toast.makeText(this, "Il n'y a pas d'item.", Toast.LENGTH_SHORT).show();
+                finish();
+                return;
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Erreur: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, v);
+        listPoint.setAdapter(adapter);
 
 
         Ecouteur ec = new Ecouteur();
         
+        quitter.setOnClickListener(ec);
 
 
+    }
 
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        instance.fermerConnexion();
+
+    }
+
+    private class Ecouteur implements View.OnClickListener {
+
+
+        @Override
+        public void onClick(View v) {
+
+            finish();
+
+        }
     }
 }
--- TP2/app/src/main/java/com/example/tp2/Jeu.java
+++ TP2/app/src/main/java/com/example/tp2/Jeu.java
@@ -1,7 +1,14 @@
 package com.example.tp2;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.DragEvent;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,7 +17,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Jeu extends AppCompatActivity {
+    DatabaseHelper instance;
     Button menuPrincipale;
+    TextView txtNbCarte, txtScoreActuel,txtCarteHG,txtCarteHD,txtCarteBG,txtCarteBD,txtC1,txtC2,txtC3,txtC4,txtC5,txtC6,txtC7,txtC8;
+    LinearLayout main,lHG,lHD,lBG,lBD,lC1,lC2,lC3,lC4,lC5,lC6,lC7,lC8;
+    Chronometer chrono;
 
 
     @Override
@@ -26,7 +37,56 @@ public class Jeu extends AppCompatActivity {
 
 
         menuPrincipale = findViewById(R.id.btnRetourMenu);
+        main = findViewById(R.id.main);
+
+        instance = DatabaseHelper.getInstance(getApplicationContext());
+        instance.ouvrirConnexion();
+        Ecouteur ec = new Ecouteur();
+
+
+        for(int i = 0; i < main.getChildCount(); i++){
+            LinearLayout colonne = (LinearLayout) main.getChildAt(i);
+            colonne.setOnDragListener(ec);
+            colonne.getChildAt(0).setOnTouchListener(ec);
+        }
+
+
+
 
 
     }
+    private class Ecouteur implements View.OnDragListener, View.OnTouchListener {
+        Drawable normal = getResources().getDrawable(R.drawable.bg_card, null);
+        Drawable select = getResources().getDrawable(R.drawable.bg_card_selectionne, null);
+
+        View carte = null;
+
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+
+
+            return true;
+        }
+
+        @Override
+        public boolean onTouch(View source, MotionEvent event) {
+
+            View.DragShadowBuilder builder = new View.DragShadowBuilder(source); //Créer une ombre du jeton
+            source.startDragAndDrop(null/*Dans le tp on peut mettre le no. de la carte*/,builder,source, 0);
+            source.setVisibility(View.INVISIBLE); //Cacher le jeton car on est en train de le déplacer
+
+            return true;
+        }
+    }
+
+
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        instance.fermerConnexion();
+    }
+
+
 }
